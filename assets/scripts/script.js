0835e8231e181f93c47b42da9289fafb434a3a07
alisabouri <alisabouri6857@gmail.com>
2025-01-03 19:00:36 +0330

    create sliddeer

---
+++ assets/scripts/script.js
@@ -0,0 +1,69 @@
+const slider = document.querySelectorAll("#slider>div");
+const  prev= document.querySelector("#next");
+const next = document.querySelector("#prev");
+const indicator = document.querySelector(".indicator");
+
+
+let flag = 0;
+let x = 0;
+let sliderLenght = slider.length;
+
+next.addEventListener("click", () => {
+  if (flag == 0 || flag < sliderLenght - 1) {
+    flag++;
+    x = x - 100;
+    slider[0].style.marginLeft = `${x}vw`;
+    cccc()
+    } else {
+    flag = 0;
+    x = 0;
+    slider[0].style.marginLeft = `${x}vw`;
+    cccc()
+  }
+});
+prev.addEventListener("click", () => {
+  if (flag > 0 && flag < sliderLenght) {
+    flag--;
+    x = x + 100;
+    slider[0].style.marginLeft = `${x}vw`;
+    cccc()
+  } else {
+    flag = sliderLenght - 1;
+    x = (sliderLenght - 1) * -100;
+    slider[0].style.marginLeft = `${x}vw`;
+    cccc()
+  }
+});
+// setInterval(() => {
+//   next.click();
+// }, 5000);
+
+function zxzx(){
+  slider.forEach((items , i) => {
+    let span = document.createElement("span");
+    span.innerHTML = i + 1;
+    span.dataset.id = i + 1
+    indicator.appendChild(span);
+   span.setAttribute('onclick' , 'zzzz(this)')
+  });
+};
+zxzx()
+function cccc(){
+const indicatorC = document.querySelectorAll(".indicator>span");
+  indicatorC.forEach((itemss , i) => {
+if(i == flag){
+  itemss.style.background = 'red'
+}else{
+  itemss.style.background = 'gray'
+
+}
+  });
+};
+function zzzz(el){
+flag = el.dataset.id -1
+x = flag * 100
+slider[0].style.marginLeft = `-${x}vw`;
+cccc()
+console.log(flag)
+}
+zzzz()
